41e764cc58bc1cbf8560b44a3a00a7cb046c56c0
CarlosAraki <carlosarak@gmail.com>
2022-11-14 22:33:13 +0000

    feat: adding env

--- django-casorio/vercel_app/settings.py
+++ django-casorio/vercel_app/settings.py
@@ -5,3 +5,16 @@ INSTALLED_APPS = [
     # ...
     'example',
 ]
+
+# vercel-django-example/vercel_app/settings.py
+# https://github.com/jayhale/vercel-django-example/blob/main/vercel_app/settings.py#L28
+
+# ...
+
+ALLOWED_HOSTS = ['.vercel.app'] # Allow *.vercel.app
+
+# ...
+
+DATABASES = {} # Prevent Django from loading an adapter
+
+# ...
